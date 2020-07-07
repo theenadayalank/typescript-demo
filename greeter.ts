b98611232357119c14611e89e7634864b9f4bc5b
Theenadayalan <puduvai.theena@gmail.com>
2020-07-07 20:28:06 +0530

    test commit

--- greeter.ts
+++ greeter.ts
@@ -17,3 +17,27 @@
   document.textContent = greeter(fullName);
 })();
 
+
+
+(function() {
+  class Student {
+      fullName: string;
+      constructor(public firstName, public middleName, public lastName) {
+          this.fullName = firstName + middleName + lastName;
+      }
+  }
+
+  interface Person {
+    firstName: string,
+    lastName: string
+  }
+
+  function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+  }
+
+  let user = new Student("Jane", "M.", "User");
+
+  document.body.textContent = greeter(user);
+})();
+
